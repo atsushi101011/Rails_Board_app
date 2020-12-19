163996e63d7bebb92456c055f710eb73db76ba39
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2020-12-19 13:33:25 +0900

    04_sorcery_install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
-
-
-
-
-  private
-    def not_authenticated
-      redirect_to login_path, alert: "Please login first"
-    end
+private
+  def not_authenticated
+    redirect_to login_path, alert: "Please login first"
+  end
 end
