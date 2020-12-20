676a81cbe7f2b6699bbc4bc47acadad1426a2531
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2020-12-20 17:14:09 +0900

    06_flash_messages

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,6 +14,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path,flash: { success: 'ログアウトしました' }
+    redirect_to root_path, flash: { success: 'ログアウトしました' }
   end
 end
