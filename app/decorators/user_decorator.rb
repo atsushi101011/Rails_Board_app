881f1cf41ea8f8a5dd6fec934b6cd9da065110d2
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2020-12-22 09:53:51 +0900

    08_board_index

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,5 +4,4 @@ class UserDecorator < Draper::Decorator
   def full_name
     "#{object.last_name} #{object.first_name}"
   end
-
 end
--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,4 @@ module RunteqNormal
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Asia/Tokyo'
   end
-
 end
