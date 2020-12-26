68a01722ab01d812bd47672bfaada6faffd1b251
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2020-12-26 10:55:13 +0900

    12_create_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,11 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     if @comment.save
-        redirect_to board_path(params[:board_id]), success: t('.success')
-      else
-        flash[:danger] = t('.fail')
-        redirect_to board_path(params[:board_id])
-      end
+      redirect_to board_path(params[:board_id]), success: t('.success')
+    else
+      flash[:danger] = t('.fail')
+      redirect_to board_path(params[:board_id])
+    end
   end
 
   private
