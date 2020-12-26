857461aed95249ed76449414df4ff60e165c1f15
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2020-12-27 07:51:19 +0900

    14_board_edit_delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path, danger: 'ログインしてください'
   end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
 end
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -26,9 +26,36 @@ class BoardsController < ApplicationController
     # @comments = Comment.all.includes(:board).order(created_at: :desc)
   end
 
+  def edit
+    if current_user == set_board.user
+      @board = Board.find(params[:id])
+    end
+  end
+
+  def update
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board), success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :edit
+    end
+  end
+
+  def destroy
+    if current_user == set_board.user
+      @board.destroy
+      redirect_to boards_path, success: t('.success')
+    end
+  end
+
   private
 
   def board_params
     params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
+  end
+
+  def set_board
+    @board =current_user.boards.find(params[:id])
   end
 end
