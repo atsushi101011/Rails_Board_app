2f39738a0b4d6950ef23eb407f77b3eb428412a9
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2020-12-27 09:04:10 +0900

    15_bookmark_board

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
+
+  def own?(object)
+    object.user_id == id
+  end
 end
