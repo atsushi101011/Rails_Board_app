1422717101d6a2a2699a5c7458628445498261af
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2021-01-04 11:30:37 +0900

    16_bookmark_ajax

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,11 @@
 class BookmarksController < ApplicationController
   def create
-    bookmark = current_user.bookmarks.build(board_id: params[:board_id])
-    bookmark.save!
-    redirect_to boards_path, success: t('.flash_bookmark')
+    @bookmark = current_user.bookmarks.build(board_id: params[:board_id])
+    @bookmark.save!
   end
 
   def destroy
-    bookmark = current_user.bookmarks.find_by(board_id: params[:board_id])
-    bookmark.destroy!
-    redirect_to boards_path, success: t('.flash_not_bookmark')
+    @bookmark = current_user.bookmarks.find_by(board_id: params[:board_id])
+    @bookmark.destroy!
   end
 end
