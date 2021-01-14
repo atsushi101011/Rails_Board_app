2bdbc60c93992cfe192248038da3a39f12e78038
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2021-01-14 12:46:36 +0900

    23_admin_user_board_crud

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,6 +1,6 @@
 Ransack.configure do |c|
-     c.add_predicate 'lteq_end_of_day',
-                     arel_predicate: 'lteq',
-                     formatter: proc {|v| v.end_of_day},
-                     compounds: false
-   end
+  c.add_predicate 'lteq_end_of_day',
+                   arel_predicate: 'lteq',
+                   formatter: proc { |v| v.end_of_day },
+                   compounds: false
+end
