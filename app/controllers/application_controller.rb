05da7ba4dbfaae9df94f8560bda68b65f14e2d5b
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2021-01-15 12:37:31 +0900

    24_error_handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,12 @@ class ApplicationController < ActionController::Base
   end
 
   def render_404
-    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    render file: Rails.root.join('public', '404.html'), status: 404, layout: false, content_type: 'text/html'
   end
 
   def render_500(error)
     logger.error(error.message)
     logger.error(error.backtrace.join("\n"))
-    render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+    render file: Rails.root.join('public', '500.html'), status: 500, layout: false, content_type: 'text/html'
   end
 end
