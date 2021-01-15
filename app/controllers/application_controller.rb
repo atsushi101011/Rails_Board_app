29625e56c768d10ada0a4b482daa51098820eca7
Atsushi Takagi (高木 淳史) <atushi10112313@gmail.com>
2021-01-15 11:36:15 +0900

    24_error_handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
+  rescue_from StandardError, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   private
 
   def not_authenticated
     redirect_to login_path, danger: 'ログインしてください'
+  end
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
+
+  def render_500(error)
+    logger.error(error.message)
+    logger.error(error.backtrace.join("\n"))
+    render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
   end
 end
